7a47af7b2fcb8fb6208ee37dafe0801153133c19
Jean Bryant Figueroa <jbster559@gmail.com>
2025-04-08 10:12:15 -0700

    Added Bookmodel with title author and price parameters

---
+++ Models/BookModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class BookModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Title is too long.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Author name is too long.")]
+        public string Author { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Price must be a positive number.")]
+        public decimal Price { get; set; }
+    }
+}
