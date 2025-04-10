b25383022ab0289460a3e4ef10715adfef7cd36d
Jean Bryant Figueroa <jbster559@gmail.com>
2025-04-09 22:14:42 -0700

    Added Rulebook model

---
+++ Models/Rulebook.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class Rulebook
+    {
+        [Required]
+        [MinLength(10)]
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public string Edition { get; set; }
+
+        [Required]
+        [Range(1990, 2030)]
+        public string Publication { get; set; }
+
+        [Required]
+        [Range(50, 1000)]
+        public string PageNumbers { get; set; }
+    }
+}
